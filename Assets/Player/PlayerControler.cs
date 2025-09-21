8e4fd3d4295fefa49229e0644e6200b891ae9868
dpman125 <108433006+dpman125@users.noreply.github.com>
2025-09-21 11:57:45 -0400

    added the boxcasting logic to the new PC

--- Assets/Player/PlayerControler.cs
+++ Assets/Player/PlayerControler.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
-    public float Speed;
-    public Vector2 MoveDirection;
+    public float movementSpeed;
+    public Vector2 moveDirection;
     public InputActionReference move;
+    private bool canMove = true;
+ 
     // public Animator Animator; can worry about this later
+
     private void OnEnable()
     {
         if (move != null && move.action != null)
@@ -28,8 +32,36 @@ public class PlayerControler : MonoBehaviour
     {
 
         //Animator.SetFloat("Speed", Mathf.Abs(MoveDirection));
-        MoveDirection = move.action.ReadValue<Vector2>();
-        transform.Translate(MoveDirection * Time.deltaTime * Speed);
+        moveDirection = move.action.ReadValue<Vector2>();
+        moveMe(moveDirection);
     }
+
+    private void moveMe(Vector2 moveVect)
+    {
+        if (canMove)
+        {
+            Vector2 direction = moveVect.normalized;
+            float distance = movementSpeed * Time.deltaTime;
+            ContactFilter2D filter = new ContactFilter2D();
+            filter.SetLayerMask(LayerMask.GetMask("StopMovement"));
+            filter.useTriggers = false;
+            RaycastHit2D[] results = new RaycastHit2D[1];
+            int hitCount = Physics2D.BoxCast(
+                transform.position,
+                new Vector2(0.8f, 0.8f),
+                0,
+                direction,
+                filter,
+                results,
+                distance
+            );
+
+            if (hitCount == 0)
+            {
+                transform.position += new Vector3(direction.x, direction.y, 0) * distance;
+            }
+        }
+    }
+
 
 }
