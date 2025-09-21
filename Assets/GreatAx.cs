c1b7ad042a1b36db096b19407f9744f2c8dc316b
hlchapman <hlc00008@mix.wvu.edu>
2025-09-21 07:06:12 -0400

    Renamed 'Item' to 'GreatAx' and created .cs file to represent it.

---
+++ Assets/GreatAx.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreatAx : MonoBehaviour, InventoryItem
+{
+    public string Name
+    {
+        get
+        {
+            return "Great Axe";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        // Later we'll want to add logic for what exactly happens when player grabs a great ax.
+        gameObject.SetActive(false);
+    }
+}
