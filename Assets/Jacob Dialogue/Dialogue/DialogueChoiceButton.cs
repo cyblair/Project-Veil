6b0dcfaed48b96e36fa507cf048503fe4b6a0d4f
Jacob Lancaster <181026957+Deetdub@users.noreply.github.com>
2025-11-03 19:15:57 -0500

    Dialogue Fixes but not done yet but started

--- Assets/Jacob Dialogue/Dialogue/DialogueChoiceButton.cs
+++ Assets/Jacob Dialogue/Dialogue/DialogueChoiceButton.cs
@@ -5,22 +5,31 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 
-public class DialogueChoiceButton : MonoBehaviour, ISelectHandler
+public class DialogueChoiceButton : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     [Header("Components")]
     [SerializeField] private Button button;
     [SerializeField] private TextMeshProUGUI choiceText;
+    private string baseText;
 
     private int choiceIndex = -1;
 
     public void SetChoiceText(string choiceTextString)
     {
+        baseText = choiceTextString;
         choiceText.text = choiceTextString;
+        choiceText.color = Color.white;
+        if (EventSystem.current.currentSelectedGameObject == this.gameObject)
+        {
+            choiceText.color = Color.yellow;
+            choiceText.text = "> " + baseText + " <";
+        }
     }
 
     public void SetChoiceIndex(int choiceIndex)
     {
         this.choiceIndex = choiceIndex;
+/*        choiceText.color = Color.white;*/
     }
 
     public void SelectButton()
@@ -31,7 +40,16 @@ public class DialogueChoiceButton : MonoBehaviour, ISelectHandler
     public void OnSelect(BaseEventData eventData)
     {
         GameEventsManager.instance.dialogueEvents.UpdateChoiceIndex(choiceIndex);
+        choiceText.color = Color.yellow;
+        choiceText.text = "> " + baseText + " <";
     }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        choiceText.color = Color.white;
+        choiceText.text = baseText;
+    }
+
 
 
 }
